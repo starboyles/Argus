4cbf7e829bb23c1d431ea78e81330c680e1ec690
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-11-29 09:53:31 +0000

    display user-specific content and improve styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,51 @@
 import { Link } from "react-router-dom";
 import { Button } from "../components/ui/button";
-import { ClerkProvider, UserButton } from "@clerk/clerk-react";
+import {
+  ClerkProvider,
+  UserButton,
+  useAuth,
+  useUser,
+} from "@clerk/clerk-react";
 
-export default function Home() {
+const Home = () => {
+  const { isSignedIn, user } = useUser();
+  if (!isSignedIn) {
+    return (
+      <div className="w-screen min-h-screen bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-gray-900 via-gray-100 to-gray-900">
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <div className="flex flex-col items-center text-center">
+            <div className="flex items-center">
+              <h1 className="mr-3 text-5xl font-extrabold">
+                Chat with Your PDF
+              </h1>
+            </div>
+            <p className="max-w-xl mt-2 text-lg text-slate-600">
+              Join hands with thousands of students and research professionals
+              to instantly answer questions & research with AI
+            </p>
+            <div className="w-full t-4">
+
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="w-screen min-h-screen bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-gray-900 via-gray-100 to-gray-900">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <div className="flex flex-col items-center text-center">
           <div className="flex items-center">
-            <h1 className="mr-3 text-5xl font-bold">Chat with your document</h1>
-            <UserButton/>
-            </div>
-            <div className="flex mt-2">
-              <Button>Go to Chats</Button>
-            </div>
+            <h1 className="mr-3 text-5xl font-extrabold">Chat Your PDF</h1>
+            <UserButton />
+          </div>
+          <div className="flex mt-2">
+            <Button>Go to Chats</Button>
+          </div>
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default Home;
