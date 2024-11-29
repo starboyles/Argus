3872ebac01dbe03a8bfbe36895a4e23ac4920540
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-11-29 13:36:58 +0000

    Center SignIn component within a full-height flex container

--- src/routes/sign-in.tsx
+++ src/routes/sign-in.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from '@clerk/clerk-react'
 
 export default function SignInPage() {
-  return <SignIn path="/sign-in" />
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <SignIn path="/sign-in" />
+    </div>
+  );
 }
