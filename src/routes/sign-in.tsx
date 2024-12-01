831ef4362a261003d6c4ef9854ce6d3067e77986
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-12-01 11:21:48 +0000

    justify

--- src/routes/sign-in.tsx
+++ src/routes/sign-in.tsx
@@ -2,7 +2,7 @@ import { SignIn } from '@clerk/clerk-react'
 
 export default function SignInPage() {
   return (
-    <div className='flex justify-center items-center h-screen'>
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <SignIn path="/sign-in" />
     </div>
   );
