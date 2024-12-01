beb684caba27ce9c6affad74b32e61e4a46e5930
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-12-01 11:22:11 +0000

    Add icons to buttons and update background color on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { Button } from "../components/ui/button";
+import {Activity, LogIn} from 'lucide-react'
 import {
   ClerkProvider,
   UserButton,
@@ -11,7 +12,7 @@ const Home = () => {
   const { isSignedIn, user } = useUser();
   if (!isSignedIn) {
     return (
-      <div className="w-screen min-h-screen bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-gray-900 via-gray-100 to-gray-900">
+      <div className="w-screen min-h-screen bg-gray-50">
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
           <div className="flex flex-col items-center text-center">
             <div className="flex items-center">
@@ -25,7 +26,9 @@ const Home = () => {
             </p>
             <div className="w-full mt-4 t-4">
               <Link to="/sign-in">
-                <Button>Get Started</Button>
+                <Button>Get Started
+                  <LogIn className="w-4 h-4 ml-1.5"/>
+                </Button>
               </Link>
             </div>
           </div>
@@ -34,15 +37,17 @@ const Home = () => {
     );
   }
   return (
-    <div className="w-screen min-h-screen bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-gray-900 via-gray-100 to-gray-900">
+    <div className="w-screen min-h-screen bg-gray-50">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <div className="flex flex-col items-center text-center">
           <div className="flex items-center">
             <h1 className="mr-3 text-5xl font-extrabold">Chat Your PDF</h1>
             <UserButton />
           </div>
-          <div className="flex mt-2">
-            <Button>Go to Chats</Button>
+          <div className="flex mt-5">
+            <Button>Go to Chats
+              <Activity className="w-4 h-4 ml-1.5"/>
+            </Button>
           </div>
         </div>
       </div>
