7421469c545b94ec8752d3f5f43ecef685b58c00
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-12-01 12:10:57 +0000

    refactor: improve code formatting

--- src/main.tsx
+++ src/main.tsx
@@ -1,39 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-// Import the layouts
-import RootLayout from './layouts/root-layout'
-import DashboardLayout from './layouts/dashboard-layout'
+import RootLayout from "./layouts/root-layout";
 
 // Import the components
-import Home from './app/page'
-import SignInPage from './routes/sign-in'
-import SignUpPage from './routes/sign-up'
-import DashboardPage from './routes/dashboard'
+import Home from "./app/page";
+import SignInPage from "./routes/sign-in";
+import SignUpPage from "./routes/sign-up";
 
 const router = createBrowserRouter([
   {
     element: <RootLayout />,
     children: [
-      { path: '/', element: <Home /> },
+      { path: "/", element: <Home /> },
 
-      { path: '/sign-in/*', element: <SignInPage /> },
-      { path: '/sign-up/*', element: <SignUpPage /> },
-      {
-        element: <DashboardLayout />,
-        path: 'dashboard',
-        children: [
-          { path: '/dashboard', element: <DashboardPage /> },
-        ],
-      },
+      { path: "/sign-in/*", element: <SignInPage /> },
+      { path: "/sign-up/*", element: <SignUpPage /> },
+      {},
     ],
   },
-])
+]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
