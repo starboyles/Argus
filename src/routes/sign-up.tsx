7e7b471bde317a37e6b1f583b81a5ba93390034a
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-12-01 11:21:58 +0000

    justify

--- src/routes/sign-up.tsx
+++ src/routes/sign-up.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/clerk-react";
 
 export default function SignUpPage() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <SignUp path="/sign-up" />
     </div>
   );
