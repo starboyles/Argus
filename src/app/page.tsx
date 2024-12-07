d93c4b1d7bb1cba86b390d27e9f2aefe8c12c59e
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-12-07 22:40:58 +0000

    feat: update home page text to reflect broader document support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,12 +13,13 @@ const Home = () => {
           <div className="flex flex-col items-center text-center">
             <div className="flex items-center">
               <h1 className="mr-3 text-5xl font-extrabold">
-                Chat with your PDF
+                Chat with your Docs
               </h1>
             </div>
             <p className="max-w-xl mt-2 text-lg text-slate-600">
-              Join thousands of developers, students & researchers to instantly
-              answer questions & research with AI
+              Argus is your AI-powered K-9 to help you sift through docs easily.
+              Join thousands of developers & researchers to instantly answer
+              questions & research with Argus.
             </p>
             <div className="w-full mt-4 t-4">
               <Link to="/sign-in">
@@ -38,12 +39,14 @@ const Home = () => {
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <div className="flex flex-col items-center text-center">
           <div className="flex items-center">
-            <h1 className="mr-3 text-5xl font-extrabold">Chat with your PDF</h1>
+            <h1 className="mr-3 text-5xl font-extrabold">
+              Chat with your Docs
+            </h1>
             <UserButton />
           </div>
           <p className="max-w-xl mt-2 text-lg text-slate-600">
             Easily upload and analyze a variety of file formats, including .pdf,
-            .docx, .ppt, & .epub 
+            .docx, .ppt, & .epub
           </p>
           <div className="w-full mt-4">
             <FileUpload />
