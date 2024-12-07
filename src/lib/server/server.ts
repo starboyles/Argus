5737294a1aa9bf9b6853759b7658ab39c2db72ae
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-12-07 13:40:23 +0000

    feat: configure dotenv to load environment variables from .env.local

--- src/lib/server/server.ts
+++ src/lib/server/server.ts
@@ -5,6 +5,7 @@ import db from '../server/db-connect.ts';
 const app = express();
 dotenv.config();
 
+dotenv.config({path: ".env.local"});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port 8000}`);
