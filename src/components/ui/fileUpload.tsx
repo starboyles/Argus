399cb2ce68926e883ac1f923906d4de4419aee2c
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-12-07 20:58:56 +0000

    feat: enhance FileUpload component to support multiple file types and multiple uploads

--- src/components/ui/fileUpload.tsx
+++ src/components/ui/fileUpload.tsx
@@ -3,7 +3,18 @@ import { useDropzone } from "react-dropzone";
 import { Inbox } from "lucide-react";
 
 const FileUpload = () => {
-  const { getRootProps, getInputProps } = useDropzone();
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: {"application/pdf": [".pdf"],
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": [".xlsx"],
+        "application/epub+zip": [".epub"],
+        "application/vnd.openxmlformats-officedocument.presentationml.presentation": [".ppt", ".pptx"]  
+    },
+    multiple: true,
+    onDrop: (acceptedFiles) => {
+        console.log(acceptedFiles);
+    }
+
+  });
   return (
     <div className="p-2 rounded-xl border-dashed border-2 cursor-pointer bg-white py-8 flex justify-center items-center flex-col">
       <div
@@ -18,8 +29,8 @@ const FileUpload = () => {
         <>
           <div className="flex flex-col items-center justify-center">
             <Inbox className="w-10 h-10 text-blue-500 " />
-            <p className="mt-4 text-sm text-slate-400">
-              Drag 'n' drop some files here, or click to select files
+            <p className="mt-4 text-sm text-slate-600">
+              Drag & drop some files here, or click to select files.
             </p>
           </div>
         </>
