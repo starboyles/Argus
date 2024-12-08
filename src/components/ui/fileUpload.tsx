c58c6380389f968a2f9559997f777f45b0c3a403
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-12-08 01:31:54 +0000

    feat: enhance file upload component with S3 integration and file size validation

--- src/components/ui/fileUpload.tsx
+++ src/components/ui/fileUpload.tsx
@@ -1,6 +1,6 @@
-import React from "react";
 import { useDropzone } from "react-dropzone";
 import { Inbox } from "lucide-react";
+import { uploadToS3 } from "../../lib/server/s3";
 
 const FileUpload = () => {
   const { getRootProps, getInputProps } = useDropzone({
@@ -14,19 +14,31 @@ const FileUpload = () => {
         [".ppt", ".pptx"],
     },
     multiple: true,
-    onDrop: (acceptedFiles) => {
+    onDrop: async (acceptedFiles) => {
       console.log(acceptedFiles);
+      const file = acceptedFiles[0];
+      if (file.size > 10 * 1024 * 1024) {
+        alert("File is too big! Max file size is 10MB");
+        return;
+      }
+
+      try {
+        const data = await uploadToS3(file);
+        console.log(data, data);
+      } catch (error) {
+        console.log(error);
+      }
     },
   });
   return (
     <div className="p-2 rounded-xl border-dashed border-2 cursor-pointer bg-white py-8 flex justify-center items-center flex-col">
       <div
-        {...getRootProps([
-          {
+        {...getRootProps(
+          [{
             className:
               "border-dashed border-2 rounded-xl bg-white py-8 flex justify-center items-center flex-col",
-          },
-        ])}
+          }],
+        )}
       >
         <input {...getInputProps()} />
         <>
