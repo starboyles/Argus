d25c3cd94622df448c52141a2ebe2a0aa154b9c6
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-12-10 16:02:46 +0000

    feat: integrate file upload with S3 and add mutation for chat creation

--- src/components/ui/fileUpload.tsx
+++ src/components/ui/fileUpload.tsx
@@ -1,8 +1,25 @@
 import { useDropzone } from "react-dropzone";
 import { Inbox } from "lucide-react";
 import { uploadToS3 } from "../../lib/server/s3";
+import axios from "axios";
+import { useMutation } from "@tanstack/react-query";
 
 const FileUpload = () => {
+  const { mutate } = useMutation({
+    mutationFn: async ({
+      file_key,
+      file_name,
+    }: {
+      file_key: string;
+      file_name: string;
+    }) => {
+      const response = await axios.post("/api/v1/create-chat", {
+        file_key,
+        file_name,
+      });
+      return response.data;
+    },
+  });
   const { getRootProps, getInputProps } = useDropzone({
     accept: {
       "application/pdf": [".pdf"],
@@ -18,13 +35,27 @@ const FileUpload = () => {
       console.log(acceptedFiles);
       const file = acceptedFiles[0];
       if (file.size > 10 * 1024 * 1024) {
-        alert("File is too big! Max file size is 10MB");
+        alert("File is too big! Max file size is 10MB. Compress!");
         return;
       }
 
       try {
         const data = await uploadToS3(file);
-        console.log(data, data);
+        if (!data.file_Key || !data.file_name) {
+          alert("Sth went wrong -- check your uploads");
+          return;
+        }
+        mutate({
+            file_key: data.file_Key,
+            file_name: data.file_name,
+          },{
+          onSuccess: (data) => {
+            console.log(data);
+          },
+          onError: (err) => {
+            console.log(err);
+          },
+        });
       } catch (error) {
         console.log(error);
       }
@@ -33,12 +64,12 @@ const FileUpload = () => {
   return (
     <div className="p-2 rounded-xl border-dashed border-2 cursor-pointer bg-white py-8 flex justify-center items-center flex-col">
       <div
-        {...getRootProps(
-          [{
+        {...getRootProps([
+          {
             className:
               "border-dashed border-2 rounded-xl bg-white py-8 flex justify-center items-center flex-col",
-          }],
-        )}
+          },
+        ])}
       >
         <input {...getInputProps()} />
         <>
