88c2657b054953b0546437845c4a5ddfebba9b79
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-12-27 08:08:32 +0000

    merging server and app.ts into 1!

--- src/lib/server/server.ts
+++ src/lib/server/server.ts
@@ -1,12 +1,24 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import db from '../server/db-connect.ts';
+import { createChat } from "../../app/api/controllers/chat-controller";
 
 const app = express();
 dotenv.config();
-
 dotenv.config({path: ".env.local"});
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port 8000}`);
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.post("/api/v1/create-chat", createChat);
+
+// Start server
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
+export default app;
