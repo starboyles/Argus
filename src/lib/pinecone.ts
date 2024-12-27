9d740ffbbd1706e61d50129f55fd31eee07e9c87
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-12-27 13:32:56 +0000

    feat: add Pinecone client initialization and S3 file loading function

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,7 +1,21 @@
 import { Pinecone } from '@pinecone-database/pinecone';
+import { downloadFromS3 } from './server/s3-server';
 
 let pinecone: Pinecone | null = null;
 
-const pc = new Pinecone({
-    apiKey: process.env.PINECONE_API_KEY!,
-});
+export const getPineconeClient = () => {
+    if (!pinecone) {
+        pinecone = new Pinecone({
+            apiKey: process.env.PINECONE_API_KEY!
+        });
+    }
+    return pinecone;
+};
+
+export async function loadS3IntoPinecone(fileKey: string) {
+    //1. Obtain the pdf. download and read from it
+    console.log("Downloading file from S3 into file system")
+    const file_name = await downloadFromS3(fileKey);
+
+
+} 
