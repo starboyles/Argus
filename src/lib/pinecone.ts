4e6f0752781c1dc415eaaa643f7b5737176007da
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-12-28 16:15:43 +0000

    feat: enhance S3 PDF loading by typing page content and metadata

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -13,14 +13,21 @@ export const getPineconeClient = () => {
   return pinecone;
 };
 
+type PDFPage = {
+  pageContent: string;
+  metadata: {
+    loc: { pageNumber: number };
+  };
+};
+
 export async function loadS3IntoPinecone(fileKey: string) {
   //1. Obtain the .pdf download and read from it
-  console.log("Downloading file from S3 into file system");
+//   console.log("Downloading file from S3 into file system");
   const file_name = await downloadFromS3(fileKey);
   if (!file_name) {
     throw new Error("Could not download file from S3");
   }
   const loader = new PDFLoader(file_name);
-  const pages = loader.load();
+  const pages = (await loader.load()) as unknown as PDFPage[];
   return pages;
 }
