1e0fb3172dfab0149c57408f8ddd9cd56c3c552c
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-12-28 16:15:53 +0000

    feat: update S3 download function to use dotenv for environment variables

--- src/lib/server/s3-server.ts
+++ src/lib/server/s3-server.ts
@@ -1,22 +1,30 @@
 import AWS from "aws-sdk";
 import fs from "fs";
+import dotenv from 'dotenv';
+
+dotenv.config();
+dotenv.config({path: ".env.local"});
 
 export async function downloadFromS3(file_key: string) {
   try {
     AWS.config.update({
-      accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY_ID,
-      secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY,
+      accessKeyId: process.env.VITE_AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.VITE_AWS_SECRET_ACCESS_KEY,
     });
     const s3 = new AWS.S3({
       region: "ap-southeast-1",
     });
+    if (!process.env.VITE_AWS_BUCKET_NAME) {
+      throw new Error("VITE_AWS_BUCKET_NAME is not defined");
+    }
     const params = {
-      Bucket: import.meta.env.VITE_AWS_BUCKET_NAME,
+      Bucket: process.env.VITE_AWS_BUCKET_NAME,
       Key: file_key,
     };
     const obj = await s3.getObject(params).promise();
-    const file_name = '/tmp/pdf-${Date.now()}.pdf'
+    const file_name = `/tmp/pdf-${Date.now()}.pdf`
     fs.writeFileSync(file_name, obj.Body as Buffer);
+    return file_name;
   } catch (error) {
     console.error(error);
     return null;
