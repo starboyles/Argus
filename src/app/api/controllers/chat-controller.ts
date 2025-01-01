b02553eaab8cef4fdc9c9ae6ec3f08551771d7e9
Kwame Sarfo <lesliegyamfi02@gmail.com>
2025-01-01 18:45:47 +0000

    feat: refactor createChat to use requireAuth middleware for user authentication

--- src/app/api/controllers/chat-controller.ts
+++ src/app/api/controllers/chat-controller.ts
@@ -3,45 +3,57 @@ import { chats } from "../../../lib/server/schema";
 import { loadS3IntoPinecone } from "../../../lib/pinecone";
 import db from "../../../lib/server/db-connect";
 import { getS3Url } from "../../../lib/server/s3";
-import { useAuth } from "@clerk/clerk-react";
+import { requireAuth } from "@clerk/express";
+import { clerkMiddleware } from '@clerk/express'
 
-export const createChat = async (req: Request, res: Response) => {
-  const { userId } = useAuth();
-  if (!userId) {
-    res.status(401).json({
-      status: "Fail",
-      message: "Unauthorized",
-    });
-    return;
+
+interface AuthenticatedRequest extends Request {
+  auth: {
+    userId: string
+  };
+}
+
+export const createChat = [
+  requireAuth(),
+  async (req: AuthenticatedRequest, res: Response) => {
+    const { userId } = req.auth;
+    if (!userId) {
+      res.status(401).json({
+        status: "Fail",
+        message: "Unauthorized",
+      });
+      return;
+    }
+    try {
+      const { file_key, file_name } = req.body;
+      console.log("file_key:", file_key);
+      console.log("file_name:", file_name);
+      const pages = await loadS3IntoPinecone(file_key);
+      const chat_id = await db.insert(chats).values({
+        fileKey: file_key,
+        pdfName: file_name,
+        pdfUrl: getS3Url(file_key),
+        userId: req.auth.userId,
+      }).
+      returning ({
+        insertedId: chats.id
+      })
+      res.status(200).json({
+        status: "success",
+        data: {
+          chat_id: chat_id[0].insertedId,
+          // file_key,
+          // file_name,
+          // pages,
+        },
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(400).json({
+        status: "Fail",
+        message: (error as Error).message,
+      });
+    }
   }
-  try {
-    const { file_key, file_name } = req.body;
-    console.log("file_key:", file_key);
-    console.log("file_name:", file_name);
-    const pages = await loadS3IntoPinecone(file_key);
-    const chat_id = await db.insert(chats).values({
-      fileKey: file_key,
-      pdfName: file_name,
-      pdfUrl: getS3Url(file_key),
-      userId: "user_id",
-    }).
-    returning ({
-      insertedId: chats.id
-    })
-    res.status(200).json({
-      status: "success",
-      data: {
-        chat_id: chat_id[0].insertedId,
-        // file_key,
-        // file_name,
-        // pages,
-      },
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(400).json({
-      status: "Fail",
-      message: (error as Error).message,
-    });
-  }
-};
+];
+
