22324e12fa955a940accfb7f78dfc00a85fd026d
Kwame Sarfo <lesliegyamfi02@gmail.com>
2025-01-01 16:16:50 +0000

    feat: update createChat to insert chat data into the database with S3 URL

--- src/app/api/controllers/chat-controller.ts
+++ src/app/api/controllers/chat-controller.ts
@@ -1,16 +1,23 @@
 import { Request, Response } from "express";
 import { chats } from "../../../lib/server/schema";
-import { schema } from "../../../lib/server/another-schema";
 import { loadS3IntoPinecone } from "../../../lib/pinecone";
+import  db  from "../../../lib/server/db-connect";
+import { getS3Url } from "../../../lib/server/s3";
+import  { useAuth } from "@clerk/clerk-react";
 
 export const createChat = async (req: Request, res: Response) => {
+  const {} = await useAuth 
   try {
-    // console.log('Full Request body:', req.body);
     const { file_key, file_name } = req.body;
     console.log("file_key:", file_key);
     console.log("file_name:", file_name);
     const pages = await loadS3IntoPinecone(file_key);
-    await schema.insert(chats).values({});
+    await db.insert(chats).values({
+      fileKey: file_key,
+      pdfName: file_name,
+      pdfUrl: getS3Url(file_key),
+      userId: "user_id",
+    });
 
     res.status(200).json({
       status: "success",
