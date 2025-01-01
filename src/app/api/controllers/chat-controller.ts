060c703f42801d82c60cb924e4e3df2305dff3b2
Kwame Sarfo <lesliegyamfi02@gmail.com>
2025-01-01 14:30:00 +0000

    feat: integrate schema insertion for chat creation in chat-controller

--- src/app/api/controllers/chat-controller.ts
+++ src/app/api/controllers/chat-controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import { chats } from "../../../lib/server/schema";
+import { schema } from "../../../lib/server/another-schema";
 import { loadS3IntoPinecone } from "../../../lib/pinecone";
 
 export const createChat = async (req: Request, res: Response) => {
@@ -8,14 +10,15 @@ export const createChat = async (req: Request, res: Response) => {
     console.log("file_key:", file_key);
     console.log("file_name:", file_name);
     const pages = await loadS3IntoPinecone(file_key);
+    await schema.insert(chats).values({});
 
     res.status(200).json({
       status: "success",
-      data: { 
-        file_key, 
+      data: {
+        file_key,
         file_name,
-        pages
-     },
+        pages,
+      },
     });
   } catch (error) {
     console.error(error);
